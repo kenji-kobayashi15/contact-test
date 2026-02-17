c88ba01cb7643a277650f6cbadacf6587490e4a5
kenji kobayashi <kenzicolab.future@gmail.com>
2026-02-17 15:39:20 +0900

    入力表示、送信機能完了

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# laravel-docker-template
+# アプリケーション名 - contact-test
+
+## 環境構築　
+
+### 1. Dockerイメージのビルド
+```bash
+docker-compose build
+```
+
+### 2. Dockerコンテナの起動
+```bash
+docker-compose up -d
+```
+
+### 3. データベースのマイグレーション
+```bash
+docker-compose exec php php artisan migrate
+```
+
+### 4. 初期データの投入（シーディング）
+```bash
+docker-compose exec php php artisan db:seed
+
+## 使用技術
+- PHP　8.1
+- Laravel　8.83.8
+- MySQL　8.0.26
+- Docker / Docker Compose
+
+## データベース設計
+
+![ER図](./er-contact-test.png)
---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,134 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+
+<div class="contact-form__content">
+    <div class="contact-form__heading">
+        <h2>Contact</h2>
+    </div>
+    <form class="form" action="/contacts/confirm" method="post">
+        @csrf
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">お名前</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="name" name="first_name" placeholder="例:山田" />
+                    <input type="name" name="last_name" placeholder="例:太郎" />
+                </div>
+                <div class="form__error">
+                    <!--バリデーション機能を実装したら記述します。-->
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">性別</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="radio" name="gender" value="男性" checked/>男性
+                    <input type="radio" name="gender" value="女性" />女性
+                    <input type="radio" name="gender" value="その他" />その他
+                </div>
+                <div class="form__error">
+                    <!--バリデーション機能を実装したら記述します。-->
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">メールアドレス</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="email" name="email" placeholder="test@example.com" />
+                </div>
+                <div class="form__error">
+                    <!--バリデーション機能を実装したら記述します。-->
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">電話番号</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="tel" name="tel1" placeholder="080" />
+                    <span>-</span>
+                    <input type="tel" name="tel2" placeholder="1234" />
+                    <span>-</span>
+                    <input type="tel" name="tel3" placeholder="5678" />
+                </div>
+                <div class="form__error">
+                    <!--バリデーション機能を実装したら記述します。-->
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">住所</span>
+                <span class="form__label--required">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="address1" placeholder="例:東京都渋谷区千駄ヶ谷123" />
+                </div>
+                <div class="form__error">
+                    <!--バリデーション機能を実装したら記述します。-->
+                </div>
+            </div>
+        </div>
+        <div class="form_group">
+            <div class="form__group-title">
+                <span class="form__label--item">建物名</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="address2" placeholder="例:千駄ヶ谷マンション101" />
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">お問い合わせ種類</span>
+                <span class="form__label--item">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--select">
+                        <select name="select_content">
+                            <option value="こんにちは">こんにちは</option>
+                            <option value="さようなら">さよなら</option>
+                            <option value="幸せですか">幸せですか</option>
+                            <option value="なぞなぞです">なぞなぞです</option>
+                        </select>
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">お問い合わせ内容</span>
+                <span class="form__label--item">※</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--textarea">
+                    <textarea name="content" placeholder="お問い合わせ内容をご記載ください"></textarea>
+                </div>
+            </div>
+        </div>
+        <div class="form__button">
+            <button class="form__button-submit" type="submit">確認画面</button>
+        </div>
+    </form>
+</div>
+@endsection
