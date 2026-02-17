153b37a97528b94ca9978655335cb8b98d92a397
kenji kobayashi <kenzicolab.future@gmail.com>
2026-02-18 01:35:57 +0900

    thanksページ修正

--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -1,16 +1,23 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="ja">
 
-@section('css')
-<link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
-@endsection
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>サンクスページ</title>
+    <link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
+</head>
 
-@section('content')
-<div class="thanks__content">
-    <div class="thanks__heading">
-        <h2>お問い合わせありがとうございます</h2>
+<body>
+    <div class="thanks__content">
+        <div class="thanks__heading">
+            <h2>お問い合わせありがとうございます</h2>
+        </div>
+
+        <div class="thanks__button">
+            <a class="thanks__button-link" href="{{ route('contact.index') }}">HOME</a>
+        </div>
     </div>
-    <div class="thanks__button">
-        <a class="thanks__button-link" href="{{ route('contact.index') }}">HOME</a>
-    </div>
-</div>
-@endsection
+</body>
+
+</html>
